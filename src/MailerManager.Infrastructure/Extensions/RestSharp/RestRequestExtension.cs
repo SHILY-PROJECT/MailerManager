295ada3ece8230591f3832133298e5d47418ec65
SHILY <iliyarock@gmail.com>
2025-02-03 23:43:36 +0300

    add extension for RestRequest

---
+++ src/MailerManager.Infrastructure/Extensions/RestSharp/RestRequestExtension.cs
@@ -0,0 +1,9 @@
+﻿using RestSharp;
+
+namespace MailerManager.Infrastructure.Extensions.RestSharp;
+
+public static class RestRequestExtension
+{
+    public static RestRequest AddQueryParameterWhereValueNotNull(this RestRequest request, string name, string? value) =>
+        string.IsNullOrWhiteSpace(value) ? request : request.AddQueryParameter(name, value);
+}

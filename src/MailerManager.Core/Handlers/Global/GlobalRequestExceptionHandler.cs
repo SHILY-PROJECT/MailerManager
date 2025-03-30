4e86bea00f1b7c87fd8210c0045745b6b5934762
SHILY <iliyarock@gmail.com>
2025-03-31 00:04:42 +0300

    Add `GlobalRequestExceptionHandler`

---
+++ src/MailerManager.Core/Handlers/Global/GlobalRequestExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace MailerManager.Core.Handlers.Global;
+
+public class GlobalRequestExceptionHandler<TRequest, TResponse, TException>(ILogger<GlobalRequestExceptionHandler<TRequest, TResponse, TException>> logger)
+    : IRequestExceptionHandler<TRequest, TResponse, TException>
+    where TRequest : BaseCommandRequest, new()
+    where TResponse : BaseCommandResult, new()
+    where TException : Exception
+{
+    public System.Threading.Tasks.Task Handle(TRequest request, TException exception, RequestExceptionHandlerState<TResponse> state, CancellationToken cancellationToken)
+    {
+        logger.LogWarning(exception, $"$SyncId={request.SyncId} Method={GetType().Name}.{nameof(Handle)} Error. RequestType: {typeof(TRequest).Name}");
+        
+        state.SetHandled(new TResponse
+        {
+            SyncId = request.SyncId,
+            IsSuccess = false,
+            Message = exception.Message
+        });
+        
+        return System.Threading.Tasks.Task.CompletedTask;
+    }
+}

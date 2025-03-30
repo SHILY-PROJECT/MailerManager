688dd64d9b2775c67da5c01cfd174a5659f630c8
SHILY <iliyarock@gmail.com>
2025-03-31 00:05:07 +0300

    Add `GlobalLoggingBehavior`

---
+++ src/MailerManager.Core/Behaviors/Global/GlobalLoggingBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using MailerManager.Core.Handlers;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MailerManager.Core.Behaviors.Global;
+
+public class GlobalLoggingBehavior<TRequest, TResponse>(ILogger<GlobalLoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : BaseCommandRequest
+    where TResponse : BaseCommandResult
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var logInfo = $"SyncId={request.SyncId}. Method={GetType().Name}.{nameof(Handle)}. RequestType={typeof(TResponse).Name} ";
+        
+        logger.LogInformation($"{logInfo}Started operation. Request={JsonSerializer.Serialize(request)}");
+
+        var sw = Stopwatch.StartNew();
+
+        var response = await next();
+        response.SyncId = request.SyncId;
+
+        sw.Stop();
+
+        logger.LogInformation($"{logInfo}Finished operation. Response={JsonSerializer.Serialize(response, new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping })}");
+        
+        return response;
+    }
+}
